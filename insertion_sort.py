0cbe6cee8df2baa508efdda09696856f6e40533f
Miles Dripps <144963802+milesdripps@users.noreply.github.com>
2025-05-18 12:23:29 -0700

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,27 @@
+def insertion_sort(arr):
+    """
+    Starting at number at index 1, we compare every number up to that index
+    If number is less than another number, we swap number down to that number
+    O(n^2) worst time complexity, since we are comparing each number to each number
+    """
+    for i in range(1, len(arr)):
+        for j in range(i):
+            if arr[i] < arr[j]: # if number is less than the first number before it
+                h, k = i, i
+                while h != k: # Swap down to the same index
+                    arr[h], arr[h-1] = arr[h-1], arr[h]
+                    h -= 1
+                break
+    return arr
+
+def main():
+    #Example lists
+    lists = [[5,1,2,9,12,3,6],
+             [200,197,190,23,200,1],
+             [9,100,22,3,44,0]
+             ]
+
+    for arr in lists:
+        print(f"{arr} --(Insertion Sort)--> {insertion_sort(arr)}")
+
+main()
